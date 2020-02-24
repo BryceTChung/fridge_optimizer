5bc2d4578e2bccb001b273721b0018773eda57f6
Bryce Chung <riceman342@gmail.com>
2020-02-24 12:47:05 -0800

    started file reading

---
+++ ingredient_class.py
@@ -0,0 +1,6 @@
+class ingredient:
+    def __init__(self):
+        self.name = "NA"
+        self.quantity = "NA"
+        self.expir_date = "NA"
+        self.tags = []
--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("hello world")
+import csv
+from ingredient_class import ingredient
+
+ingre_list = []
+
+with open("ingredients.csv", "r") as csv_file:
+    reader = csv.DictReader(csv_file)
+    for i in reader:
+        ingr = ingredient()
+        ingr.name = i["name"]
+        ingr.quantity = i["quantity"]
+        ingr.expir_date = i["expir_date"]
+        tag_string = i["tags"]
+        tag = ""
+        for x in tag_string:
+            if x == ',':
+                ingr.tags.append(tag)
+                tag = ""
+            tag = tag + x
+        #ingr.tags = i["tags"]
+        ingre_list.append(ingr)
+    for j in ingre_list:
+        print(j.name)
+        print(j.expir_date)
+        print(j.tags)
