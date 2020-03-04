ac3fab1fb5ab763b4f3b0be00bf3c6012786722a
Bryce Chung <brycetchung@gmail.com>
2020-03-03 18:44:34 -0800

    fixed reading of tags into array not recognizing an empty list and appending last tag

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import csv
 from ingredient_class import ingredient
 
 ingre_list = []
-
 with open("ingredients.csv", "r") as csv_file:
     reader = csv.DictReader(csv_file)
     for i in reader:
@@ -11,12 +10,18 @@ with open("ingredients.csv", "r") as csv_file:
         ingr.quantity = i["quantity"]
         ingr.expir_date = i["expir_date"]
         tag_string = i["tags"]
-        tag = ""
-        for x in tag_string:
-            if x == ',':
-                ingr.tags.append(tag)
-                tag = ""
-            tag = tag + x
+        tag = "" 
+        while True:
+            if type(tag_string) == type(None):
+                break
+            for x in tag_string:
+                if x == ',':
+                    ingr.tags.append(tag)
+                    tag = ""
+                    x=''
+                tag = tag + x
+            ingr.tags.append(tag)
+            break
         #ingr.tags = i["tags"]
         ingre_list.append(ingr)
     for j in ingre_list:
